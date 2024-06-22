453c49ba6ef878864b4f4b1892ce40367878e979
mohamad-ikhfan <mohamad.ikhfan@gmail.com>
2024-06-22 21:19:14 +0700

    upload

--- app/Filament/Resources/UserManagementResource.php
+++ app/Filament/Resources/UserManagementResource.php
@@ -68,11 +68,7 @@ class UserManagementResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteAction::make(),
             ]);
     }
 
